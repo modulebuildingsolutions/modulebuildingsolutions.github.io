691682aa55ce4f849bdf826a654ae7a63ab2632e
Pierce <piercemiller23@gmail.com>
2020-10-20 16:56:59 +1300

    buttons updated

---
+++ js/script.js
@@ -0,0 +1,23 @@
+window.sr = ScrollReveal({duration: 1500});
+sr.reveal('.site-content .d-flex');
+sr.reveal('.section-1 .card');
+sr.reveal('.section-2 .d-flex');
+sr.reveal('.section-3 .col-md-4');
+sr.reveal('.section-4 .col-md-5 .col-md-7');
+
+function contact() {
+    window.location.href = "contact.html";
+  }
+
+function howItWorks() {
+    window.location.href = "about.html";
+}
+
+function home() {
+    window.location.href = "index.html";
+}
+
+function faq() {
+    window.location.href = "faq.html";
+}
+
